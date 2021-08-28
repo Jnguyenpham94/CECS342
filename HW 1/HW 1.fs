6a869bf6d9d7622a0ff6b9ac39ce592c1bd8322c
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-08-28 16:28:53 -0700

    HW 1 check if prime comments and input statement

--- HW 1/HW 1.fs
+++ HW 1/HW 1.fs
@@ -1,3 +1,8 @@
 open System
+//program to check if inputted int is prime
 
-let x = 5
+printf "Input a value to check if prime: "
+let x = Console.ReadLine() |> int
+//boolean to stop looping when divisor is found
+let found = false
+

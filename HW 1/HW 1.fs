83cf5149f768835de9c736193ac377d7861f4200
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-08-28 22:24:52 -0700

    need to iterate over possible values in while loop

--- HW 1/HW 1.fs
+++
@@ -1,8 +0,0 @@
-open System
-//program to check if inputted int is prime
-
-printf "Input a value to check if prime: "
-let x = Console.ReadLine() |> int
-//boolean to stop looping when divisor is found
-let found = false
-
---
+++ HW 1/isPrime.fs
@@ -0,0 +1,15 @@
+﻿
+open System
+//program to check if inputted int is prime
+
+printfn "Input a value to check if prime: "
+let x = Console.ReadLine() |> int
+//boolean to stop looping when divisor is found
+let found = false
+
+if x % 2 = 0 then
+    printfn "Even so NOT PRIME"
+else
+    let mutable i = 0
+    while i < x && found = false do
+        i <- i + 1 // <- is for reassignment / mutation

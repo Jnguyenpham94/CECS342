f10e567bfbf675dee49ad285f57b0d755795a8cc
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-08-29 18:27:05 -0700

    working on while loop

--- HW 1/isPrime.fs
+++ HW 1/isPrime.fs
@@ -5,15 +5,17 @@ open System
 printfn "Input a value to check if prime: "
 let x = Console.ReadLine() |> int
 //boolean to stop looping when divisor is found or just to stop while loop for special cases
-let mutable found = false
+let mutable stop = false
 
-if x % 2 = 0 || x = 1 then
+if x % 2 = 0 then
     printfn "Even so NOT PRIME"
 elif x = 2 then
     printfn "Is 2 which is PRIME"
 else
-    let mutable i = 3 //all even numbers are not prime so start at first non-prime int
-    while i < x || found = false do
-        if i = sqrt x then
-            found = true
+    let mutable i = 1 //all even numbers are not prime so start at first non-prime int
+    while i < x || stop <> false do
+        if i > sqrt x then
+            stop = true
+        if x % i = 0 then
+            stop = true
         i <- i + 2 // <- is for reassignment / mutation

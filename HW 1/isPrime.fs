3703bcd4cc6d7cc735f666dd8877472e1b2607d7
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-09-02 05:10:01 -0700

    update on if statements

--- HW 1/isPrime.fs
+++ HW 1/isPrime.fs
@@ -6,19 +6,22 @@ printfn "Input a value to check if prime: "
 let x = Console.ReadLine() |> int
 //boolean to stop looping when divisor is found or just to stop while loop for special cases
 let mutable stop = false
+let mutable found = false
 
 if x % 2 = 0 then
     printfn "%d is even so NOT PRIME" x
+elif x <= 1 then
+    printfn "%d is NOT PRIME" x
 else
-    let mutable i = 1 //all even numbers are prime so start at first non-prime int
+    let mutable i = 3 //all even numbers are prime so start at first non-prime int after 1
     while i < x && stop <> true do
-        if float i > sqrt (float x) then //check for largest int that can be divisor or x
-            stop <- true
-            printfn "%d NOT PRIME" x
+        if float i >= sqrt (float x) then //check for largest int that can be divisor of x
+            stop <- true //if greater NOT PRIME
         elif x % i = 0 then
-            stop <- true//TODO: need to get this output cases done
-            printfn "%d is NOT PRIME" x
-        else
             stop <- true
-            printfn "%d is PRIME" x
-        i <- i + 2 // iterate 2 values to skip even values
+        else
+            i <- i + 2 // iterate 2 values to skip even values
+    if found = true then
+        printfn "%d is PRIME" x
+    else
+        printfn "%d is NOT PRIME" x

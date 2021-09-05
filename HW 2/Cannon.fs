a94a5c8ab645b0c2889ee9a50ab9e9808ee5b248
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-09-05 03:47:38 -0700

    comments and getAngle in progress

--- HW 2/Cannon.fs
+++ HW 2/Cannon.fs
@@ -1,4 +1,35 @@
 open System
 
+// returns float value 0 - 1000 m inclusive.
 let placeTarget () = 
     ()
+
+//prompts user to enter angle of fire
+let getAngle  =
+    printfn "Enter angle between 0 - 90"
+    let mutable amount = Console.ReadLine |> int
+    while amount < 0 && amount > 90 do
+        printfn "Angle is out of bounds. Enter angle between 0 - 90"
+        amount <- Console.ReadLine |> int
+    float amount
+
+//prompts user for an angle to fire cannon
+let getGunpowder () =
+    ()
+
+//angle of amount of gunpowder
+//returns horizontal distance of projectile
+//no prints
+let calculateDistance angle gunpowder =
+    ()
+
+//location of target and distance projectile has moved
+//hit occurs if projectile lands within 1.0 m of target
+//no let and prints allowed
+let isHit location distance =
+    ()
+
+//main
+[<EntryPoint>]
+let main argv =
+    printfn "main stuff here"

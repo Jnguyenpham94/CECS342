c93d65c3f2228c919b63d5c949ec7aec5ddb8fed
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-09-05 15:04:23 -0700

    placeTarget in Progress

--- HW 2/Cannon.fs
+++ HW 2/Cannon.fs
@@ -1,11 +1,17 @@
+(* text-based game in which the player attempts to shoot cannonballs at a target that is some
+distance away. The player repeatedly enters an angle to fire the cannon at and an amount of gunpowder
+to use. The game calculates how far the cannonball will fly under those parameters and continues until the
+cannonball falls within 1 meter of the target. 
+*)
 open System
 
 // returns float value 0 - 1000 m inclusive.
 let placeTarget () = 
-    ()
+    let rand = (new Random()).NextDouble()
+    rand
 
 //prompts user to enter angle of fire
-let getAngle  =
+let getAngle  () =
     printfn "Enter angle between 0 - 90"
     let mutable amount = Console.ReadLine |> int
     while amount < 0 && amount > 90 do
@@ -33,3 +39,5 @@ let isHit location distance =
 [<EntryPoint>]
 let main argv =
     printfn "main stuff here"
+
+    0

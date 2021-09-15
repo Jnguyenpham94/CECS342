88499db44c934e74773c762efb14e8a2210b28af
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-09-15 03:09:34 -0700

    comments included for imp

--- HW 2/Program.fs
+++ HW 2/Program.fs
@@ -1,15 +1,24 @@
-﻿
+﻿//no prints in functions below and no input from user only from parameters
 open System
 
 type VendingMachine = {name: string; inventory: int; price: float}
 
-//no prints in functions below and no input from user only from parameters
+(*takes VendingMachine
+returns string
+NO IF STATEMENTS ALLOWED
+*)
 let machineDescription machine =
     ()
 
+(* returns true if person with dollars amount is able to purchase count items from given machine
+NEEDS TO BE DONE IN 1 STATEMENT!!!
+*)
 let canPurchase machine count dollars =
     ()
 
+(*checks to see if the given dollars can purchase the requested count of items from the machine
+returns tuple
+*)
 let purchase machine count dollars =
     ()
 

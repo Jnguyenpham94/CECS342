586ba332f010b1e8474ce7e6f57c7b5682db35c9
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-09-15 02:42:40 -0700

    file directory update

--- HW 1/obj/Release/net5.0/HW 1.AssemblyInfo.fs
+++ HW 1/obj/Release/net5.0/HW 1.AssemblyInfo.fs
@@ -7,11 +7,11 @@ open System
 open System.Reflection
 
 
-[<assembly: System.Reflection.AssemblyCompanyAttribute("HW 1 cop")>]
+[<assembly: System.Reflection.AssemblyCompanyAttribute("HW 1")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Release")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
 [<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
-[<assembly: System.Reflection.AssemblyProductAttribute("HW 1 cop")>]
-[<assembly: System.Reflection.AssemblyTitleAttribute("HW 1 cop")>]
+[<assembly: System.Reflection.AssemblyProductAttribute("HW 1")>]
+[<assembly: System.Reflection.AssemblyTitleAttribute("HW 1")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
 do()

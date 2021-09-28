ce83f34228ea2fbf9c7a1af96105f43773cc74fe
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2021-09-27 23:01:50 -0700

    withdraw in progress

--- HW 3/BankAccount.fs
+++ HW 3/BankAccount.fs
@@ -7,11 +7,15 @@ type AccountStatus =
     | Balance of int
     | OverDrawn of int
 
-type BankAccount = {name: string; account: AccountStatus; creditLimit: int option }
+type BankAccount = {name: string; account: AccountStatus; creditLimit: int option}
 
+//TODO: withdraw function
 let withdraw bankAccount requestInt =
     ()
 
 [<EntryPoint>]
 let main argv =
+    let m_burns = {name = "Montgomery Burns"; account = Balance 100000; creditLimit = Some 10000}
+    let neal = {name = "Neal Terrell"; account = OverDrawn 100; creditLimit = None}
+    let bob = {name = "Robert Dugalle"; account = Empty 0; creditLimit = Some 1000}
     0 // return an integer exit code

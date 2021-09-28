66d75aba380756cf8e13db2649f5a0d8a10429fe
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2021-09-28 03:09:48 -0700

    a bit closer need more testing
    Empty and Balance especially. Overdrawn maybe a bit more

--- HW 3/BankAccount.fs
+++ HW 3/BankAccount.fs
@@ -8,18 +8,22 @@ type AccountStatus =
 
 type BankAccount = {name: string; account: AccountStatus; creditLimit: int option}
 
-//TODO: withdraw function
+//TODO: withdraw function overdrawn done EMPTY and Balance
 let withdraw bankAccount (requestInt : int) =
     match bankAccount.account with
     | OverDrawn o -> (requestInt,{name = bankAccount.name; account = bankAccount.account; creditLimit = bankAccount.creditLimit})
     | Empty e -> (requestInt,{name = bankAccount.name; account = bankAccount.account; creditLimit = bankAccount.creditLimit})
-    | Balance b -> (requestInt,{name = bankAccount.name; account = bankAccount.account; creditLimit = bankAccount.creditLimit})
+    | Balance b -> (b - requestInt,{name = bankAccount.name; account = bankAccount.account; creditLimit = bankAccount.creditLimit})
 
 [<EntryPoint>]
 let main argv =
     let m_burns = {name = "Montgomery Burns"; account = Balance 100000; creditLimit = Some 10000}
     let neal = {name = "Neal Terrell"; account = OverDrawn 900; creditLimit = None}
     let bob = {name = "Robert Dugalle"; account = Empty 0; creditLimit = Some 1000}
-    let result = withdraw neal 100
-    printfn "%O" result
+    let result1 = withdraw m_burns 100
+    let result2 = withdraw neal 100
+    let result3 = withdraw bob 100
+    printfn "%O" result1
+    printfn "%O" result2
+    printfn "%O" result3
     0 // return an integer exit code

9df42a12f01093e8bc03af619b2f40ec53314ec6
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2021-10-26 02:28:28 -0700

    isWealthy 1/7 funcs DONE
    function comments created

--- HW 4/Program.fs
+++ HW 4/Program.fs
@@ -7,24 +7,49 @@ type AccountStatus =
 
 type BankAccount = {name: string; account: AccountStatus; creditLimit: int option}
 
+//take 1 BankAccount 
+//RETURNS true if account Balance > 100,000
+//PREDICATE
 let isWealthy bankAccount =
-    ()
+    match bankAccount.account with
+    |Balance e -> if e > 100000 then
+                    true
+                  else
+                    false
+    | _ -> false
 
+//list of BankAccounts 
+//RETURNS new list containing only accounts that are OverDrawn
+//HINT: List.filter
+//PREDICATE
 let findOverdrawn bankAccount =
     ()
 
+//2 BankAccounts
+//RETURNS whichever has larger $. OverDrawn : negative $ Empty: 0
+//HINT: write helper to convert account to int
+//PREDICATE
 let largerAmount a b =
     ()
 
+//list of BankAccounts
+//RETURNS new list containing $ 
+//HINT: List.map
 let accountAmounts bankAccounts =
     ()
 
-let amountsWhere =
+//predicate of tpe (BankAccount->bool), list of BankAccounts
+//RETURNS new list of $ amounts of accounts that satisfy(true) the predicate
+let amountsWhere predicate bankAccounts =
     ()
 
+//list of AccountStatus
+//RETURNS sum of $ amounts: Empty is 0; OverDrawn with abs of $; Balance other
 let combineAccounts accountStatus =
     ()
 
+//list of BankAccounts
+//RETURNS largest $ BankAccount obj; if 2 have same amount RETURNS first one
 let wealthiestAccount bankAccount =
     ()
 
@@ -34,5 +59,8 @@ let main argv =
     let dave = {name = "Dave Davidson"; account = OverDrawn 200; creditLimit = None}
     let tom = {name = "Tom Thompson"; account = Balance 200000; creditLimit = Some 500}
     let jackie = {name = "Jackie Jackson"; account = Empty 0; creditLimit = None}
+
+    isWealthy neal |> printfn "%O" //false
+    isWealthy tom |> printfn "%O" //true
 
     0
